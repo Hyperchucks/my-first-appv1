c8c0493649cc64f79e917860af9ff6111d662a2d
Antonio Marrero Bonilla <gaming_marrero@yahoo.com>
2017-12-30 20:49:59 -0500

    Revert Component Test Servers
    Deleted Test Servers Components for redo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { TestserverComponent } from './testserver/testserver.component';
+
 
 
 @NgModule({
@@ -13,7 +13,7 @@ import { TestserverComponent } from './testserver/testserver.component';
     AppComponent,
     ServerComponent,
     ServersComponent,
-    TestserverComponent
+    
   ],
   imports: [
     BrowserModule,
