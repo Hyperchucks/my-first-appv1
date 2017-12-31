e68a05aa00e9787b6b83c1b58c603a81367aa5c3
Antonio Marrero Bonilla <gaming_marrero@yahoo.com>
2017-12-31 17:24:15 -0500

    Update from Crash
    Updated angular CLI and made a horizontal line to push changes.

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>
+    WARNING ALERT!!!!! BECAREFUL BACK OUT!!!!
+  </p>
+  `,
+  styles: [`
+  p {
+    padding: 20px;
+    background-color: mistyrose;
+    border: 1px solid red;
+    
+}
+    `
+  ]
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+}
