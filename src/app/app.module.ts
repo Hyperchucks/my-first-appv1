eb8214d66afca8499af9b53b84faffc06d9cbdef
Antonio Marrero Bonilla <gaming_marrero@yahoo.com>
2017-12-30 21:39:49 -0500

    Component Creation Assignment
    Create two new components: WarningAlert & SuccessAlert. Provide output beneath each other in the AppComponent. Style the Components appropiately with red and green background and color text.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './WarningAlert/warningalert.component';
 
 
 @NgModule({
@@ -13,11 +15,13 @@ import { ServersComponent } from './servers/servers.component';
     AppComponent,
     ServerComponent,
     ServersComponent,
-    
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
