c13a507744395c4205ef872248326a18e13b19ac
Antonio Marrero Bonilla <gaming_marrero@yahoo.com>
2018-01-28 03:22:19 -0500

    Finish forms of databidning

--- src/app/forms-of-databinding/forms-of-databinding.component.ts
+++ src/app/forms-of-databinding/forms-of-databinding.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forms-of-databinding.component.css']
 })
 export class FormsOfDatabindingComponent implements OnInit {
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
